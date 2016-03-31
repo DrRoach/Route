b591d67a3d88a5b00d49956fb2e1f531fb0a08c0
Ryan Deas <ryandeas1@gmail.com>
2016-03-31 19:26:06 +0100

    Added comment explaining custom routing

--- CustomRoutes.php
+++ CustomRoutes.php
@@ -4,6 +4,15 @@ class CustomRoutes
 {
     public static function all()
     {
+        /**
+         * All custom routes should follow the same format:
+         * The key of the array is the URL to look for, this can be anything that you like. Any
+         * trailing backslashes on both the URL in this file and the URL visited by the user
+         * will be ignored. The value of the array is the Controller and function that should be
+         * loaded. Notice how you write it the same way you would a static function call.
+         * Example:
+         * '/friends/delete' => 'EnemiesController::add()'
+         */
         return [
             '/' => 'HomeController::index()'
         ];
