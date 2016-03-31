06d7d25dfc6e359cd467172fd59defdafa1531ff
Ryan Deas <ryandeas1@gmail.com>
2016-03-31 19:39:49 +0100

    Created Custom 404 page

---
+++ 404.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="404.css">
+</head>
+<body>
+    <h1>404</h1>
+    <h2>Sorry, the page you're looking for couldn't be found.</h2>
+</body>
+</html>
